7b97a293ea3befde490d88482856c90ea43b86fa
Rakesh <28902301+yukthr@users.noreply.github.com>
2021-11-17 13:14:11 +0000

    Update server.py

--- app/server.py
+++ app/server.py
@@ -9,7 +9,7 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
+export_file_url = 'https://www.dropbox.com/s/eqqimqbkooyuq8w/export.pkl?dl=1'
 export_file_name = 'export.pkl'
 
 classes = ['airplane', 'automobile', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship' ,'truck']
